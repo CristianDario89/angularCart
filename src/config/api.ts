27c15b03ae00429e04352322a57cef1bec1d22e0
Cristian <ccristiandarioromero@gmail.com>
2020-10-22 04:25:53 -0300

    adding gh-pages

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 import { environment } from 'src/environments/environment'
 
-export const baseUrl = environment.production ? 'https://carritoangular.netlify.app' : 'http://localhost:3000'
+export const baseUrl = environment.production ? 'https://cristiandario89.github.io/angularCart/' : 'http://localhost:3000'
 export const productsUrl = baseUrl + '/products'
 export const cartUrl = baseUrl + '/cart'
